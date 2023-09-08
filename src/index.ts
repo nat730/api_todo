e21e4ec6372be11cd5b6936781ac05a050d248be
nat730 <natanaelcolart@gmail.com>
2023-09-08 16:00:00 +0200

    debut sql lite

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express from 'express';
+import Sequelize from './sequelize';
 const app = express();
 const port = 3000;
 
@@ -10,3 +11,11 @@ app.get('/api/:data', (req, res) => {
 app.listen(port, () => {
   console.log(`API listening at http://localhost:${port}`);
 });
+
+Sequelize.sync()
+  .then(() => {
+    console.log('Base de données synchronisée avec succès.');
+  })
+  .catch((erreur) => {
+    console.error('Erreur lors de la synchronisation de la base de données :', erreur);
+  });
