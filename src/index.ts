cd36679b05cafad9c95603e9b154c530ee141787
nat730 <natanaelcolart@gmail.com>
2023-09-11 11:27:56 +0200

    2023-09-11 - .

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 import express from 'express';
-import Sequelize from './sequelize';
+
 const app = express();
 const port = 3000;
 
-app.get('/api/:data', (req, res) => {
-  const data = req.params.data;
-  res.send(`Bonjour ${data}`);
+app.get('/api/:taskText', (req, res) => {
+  const data = req.params.taskText;
+  res.send(`Texte de la tâche reçu : ${data}`);
 });
 
-app.listen(port, () => {
-  console.log(`API listening at http://localhost:${port}`);
+const server = app.listen(port, () => {
+  console.log(`API en écoute sur http://localhost:${port}`);
 });
 
-Sequelize.sync()
-  .then(() => {
-    console.log('Base de données synchronisée avec succès.');
-  })
-  .catch((erreur) => {
-    console.error('Erreur lors de la synchronisation de la base de données :', erreur);
-  });
+export { server };
--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,8 +1,38 @@
-import { Sequelize } from 'sequelize';
+  import { DataTypes, Sequelize } from "sequelize";
+  import { server } from "./index";
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./db.sqlite",
-})
+  const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./db.sqlite",
+  });
 
-export default sequelize;
+  const Todo = sequelize.define("Todo", {
+    name: {
+      type: DataTypes.STRING,
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+    },
+  }, {
+    timestamps: false,
+  });
+
+  sequelize
+    .sync({ force: true })
+    .then(() => {
+      console.log('Synchronisation de la base de données réussie.');
+      Todo.create({
+        name: "Tâche 1",
+        status: true,
+      })
+      .then((todo) => {
+        console.log("Tâche créée :", todo.get());
+        Todo.findAll().then((todos) => {
+          console.log("Toutes les tâches :", todos.map(todo => todo.get()));
+        });
+      });
+    })
+    .catch(error => {
+      console.error('Erreur de synchronisation :', error);
+    });
+
